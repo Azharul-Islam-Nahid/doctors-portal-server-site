8a7c171ab2309a862b81922273157a39f7769303
Azharul Islam <lonewolfazharul@gmail.com>
2022-11-19 20:53:12 +0600

    user apis admin verify middleware

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ async function run() {
 
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
+        const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+
+
+        //NOTE: make sure to run after jwt verification
+
+        const verifyAdmin = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            next();
+        }
+
+
 
         function verifyJWT(req, res, next) {
             const authHeader = req.headers.authorization;
@@ -67,52 +85,68 @@ async function run() {
             })
             res.send(options);
 
-            // app.get('/v2/getAppointmentOptions', async (req, res) => {
-            //     const date = req.query.date;
-            //     const options = await appointmentOptionCollection.aggregate([
-            //         {
-            //             $lookup: {
-            //                 from: 'bookings',
-            //                 localField: 'name',
-            //                 foreignField: 'treatment',
-            //                 pipeline: [
-            //                     {
-            //                         $match: {
-            //                             $expr: {
-            //                                 $eq: ['$apointmentDate', date]
-            //                             }
-            //                         }
-            //                     }
-            //                 ],
-            //                 as: 'booked'
-            //             }
-            //         },
-            //         {
-            //             $project: {
-            //                 name: 1,
-            //                 slots: 1,
-            //                 booked: {
-            //                     $map: {
-            //                         input: '$booked',
-            //                         as: 'book',
-            //                         in: '$book.slot'
-            //                     }
-            //                 }
-            //             }
-            //         },
-            //         {
-            //             $project: {
-            //                 name: 1,
-            //                 slots: {
-            //                     $setDifference: ['$slots', '$booked']
-            //                 }
-            //             }
-            //         }
-            //     ]).toArray();
-            //     res.send(options);
-            // })
 
         })
+
+
+
+
+        // app.get('/v2/getAppointmentOptions', async (req, res) => {
+        //     const date = req.query.date;
+        //     const options = await appointmentOptionCollection.aggregate([
+        //         {
+        //             $lookup: {
+        //                 from: 'bookings',
+        //                 localField: 'name',
+        //                 foreignField: 'treatment',
+        //                 pipeline: [
+        //                     {
+        //                         $match: {
+        //                             $expr: {
+        //                                 $eq: ['$apointmentDate', date]
+        //                             }
+        //                         }
+        //                     }
+        //                 ],
+        //                 as: 'booked'
+        //             }
+        //         },
+        //         {
+        //             $project: {
+        //                 name: 1,
+        //                 slots: 1,
+        //                 booked: {
+        //                     $map: {
+        //                         input: '$booked',
+        //                         as: 'book',
+        //                         in: '$book.slot'
+        //                     }
+        //                 }
+        //             }
+        //         },
+        //         {
+        //             $project: {
+        //                 name: 1,
+        //                 slots: {
+        //                     $setDifference: ['$slots', '$booked']
+        //                 }
+        //             }
+        //         }
+        //     ]).toArray();
+        //     res.send(options);
+        // })
+
+
+
+
+        app.get('/appointmentSpecialty', async (req, res) => {
+            const query = {}
+            const result = await appointmentOptionCollection.find(query).project({ name: 1 }).toArray();
+            res.send(result);
+        })
+
+
+
 
         // ***
         // API Naming Convention
@@ -167,7 +201,7 @@ async function run() {
                 return res.send({ accessToken: token });
             }
             console.log(user);
-            res.status(403).send({ accessToken: 'PEEKABOOOOOOOOOOOOOOOOOOOOOOO!' });
+            return res.status(403).send({ accessToken: 'PEEKABOOOOOOOOOOOOOOOOOOOOOOO!' });
         })
 
 
@@ -180,15 +214,7 @@ async function run() {
 
 
 
-        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const query = { email: decodedEmail };
-            const user = await usersCollection.findOne(query);
-
-            if (user?.role !== 'admin') {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-
+        app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
@@ -215,6 +241,28 @@ async function run() {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
+        })
+
+
+        app.post('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+
+        app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors);
+        })
+
+
+        app.delete('/doctors/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await doctorsCollection.deleteOne(filter);
+            return res.send(result);
         })
 
     }
